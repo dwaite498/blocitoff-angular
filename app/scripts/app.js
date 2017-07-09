6c4d86d53bc67159ecdde767c062c7be5ef22dab
Davey <dwaite498@gmail.com>
2017-07-09 15:02:30 -0400

    prepare home controller

---
+++ app/scripts/app.js
@@ -0,0 +1,19 @@
+(function() {
+  function config($locationProvider, $stateProvider) {
+    $locationProvider
+      .html5Mode({
+        enabled: true,
+        requireBase: false
+      });
+
+    $stateProvider
+      .state('home', {
+        url: '/',
+        controller: 'HomeCtrl as home',
+        templateURL: '/templates/home.html'
+      });
+  }
+
+  angular
+    .module('blocitoffAngular', ['ui.router', 'firebase'])
+})();
---
+++ app/scripts/controllers/HomeCtrl.js
@@ -0,0 +1,8 @@
+(function() {
+  function HomeCtrl() {
+  }
+
+  angular
+    .module('blocitoffAngular')
+    .controller('HomeCtrl', [HomeCtrl]);
+})();
